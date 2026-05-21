53fdba0a6807c2abaddebffb62fb7c6e50aee5e9
dilipmhj <maharjandilip103@gmail.com>
2026-05-21 13:43:04 +0545

    create booking route, controller and services

---
+++ src/services/booking.service.js
@@ -0,0 +1,53 @@
+import Booking from "../models/Booking.js";
+import {
+  BOOKING_STATUS_CONFIRMED,
+  BOOKING_STATUS_CANCELED,
+} from "../constants/bookingStatus.js";
+
+const getBookings = async () => {
+  return await Booking.find().sort({ createdAt: -1 });
+};
+const getBookinByID = async (Id) => {
+  return await Booking.findById(Id);
+};
+
+const createBooking = async (userId, roomId, vehicleId, startDate, endDate) => {
+  return await Booking.create(userId, roomId, vehicleId, startDate, endDate);
+};
+const updateBookingStatus = async (id, status) => {
+  return await Booking.findByIdAndUpdate(id, { status }, { new: true });
+};
+
+const cancelBooking = async (id) => {
+  return await Booking.findByIdAndUpdate(
+    id,
+    { status: BOOKING_STATUS_CANCELED },
+    { new: true },
+  );
+};
+const deleteBooking = async (id) => {
+  return await Booking.findByIdAndDelete(id);
+};
+
+//payment
+const confirmedBooking = async (id) => {
+  return await Booking.findByIdAndUpdate(
+    id,
+    { status: BOOKING_STATUS_CONFIRMED },
+    { new: true },
+  );
+};
+const getBookingsByUser = () => {};
+const getBookingsByOwner = () => {};
+
+export default {
+  getBookings,
+  getBookinByID,
+  createBooking,
+  updateBookingStatus,
+  cancelBooking,
+  deleteBooking,
+  confirmedBooking,
+  getBookingsByUser,
+  getBookingsByOwner,
+};
