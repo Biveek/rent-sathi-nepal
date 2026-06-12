9837f2beedf8ed6131cd39f0a6face773bef6f98
dilipmhj <maharjandilip103@gmail.com>
2026-06-12 18:45:07 +0545

    create navbar

---
+++ client/src/constants/routes.js
@@ -0,0 +1,28 @@
+export const HOME_ROUTE="/";
+export const ROOMS_ROUTE="/rooms";
+export const VEHICLES_ROUTE="/vehicles";
+export const SERVICES_ROUTE="/services";
+export const CONTACT_ROUTE="/contact";
+
+export const navMenu = [
+    {
+        label: "Home",
+        route:HOME_ROUTE,
+    },
+    {
+        label: "rooms",
+        route:ROOMS_ROUTE,
+    },
+    {
+        label: "vehicles",
+        route:VEHICLES_ROUTE,
+    },
+    {
+        label: "services",
+        route:SERVICES_ROUTE,
+    },
+    {
+        label: "contact",
+        route:CONTACT_ROUTE,
+    },
+]
